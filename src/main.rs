5acbca226166b750239b66b43e4b1d72a51671b1
Ryan Jacobs <rjacobs@cs.utexas.edu>
2020-01-18 02:48:34 -0600

    ready for release

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 extern crate clap;
 use std::process::{Command, Stdio};
 
+pub static EXIT_QEMU_SUCCESS: i64 = (5 << 1) | 1;
+pub static EXIT_QEMU_SIGNAL: i64 = (4 << 1) | 1;
+
+
 #[derive(Clap)]
 #[clap(version = "1.0", author = "Ryan Jacobs")]
 struct Opts {
@@ -16,7 +20,9 @@ enum SubCommand {
     #[clap(name = "run", version = "1.0", author = "Ryan Jacobs")]
     Run,
     #[clap(name = "runner", version = "1.0", author = "Ryan Jacobs")]
-    Runner(Runner)
+    Runner(Runner),
+    #[clap(name = "clean", version = "1.0", author = "Ryan Jacobs")]
+    Clean,
 }
 
 #[derive(Clap)]
@@ -27,24 +33,26 @@ struct Build {
 
 #[derive(Clap)]
 struct Runner {
-    #[clap(required = true, min_values = 1)]
+    #[clap(short = "p", long = "path")]
     path: String
 }
 
 fn main() {
     let opts = Opts::parse();
-    match opts.subcmd {
+    let exit_code = match opts.subcmd {
         SubCommand::Build(build) => handle_build(build),
         SubCommand::Run => handle_run(),
-        SubCommand::Runner(_) => handle_runner()
-    }
+        SubCommand::Runner(runner) => handle_runner(runner),
+        SubCommand::Clean => handle_clean(),
+    };
+    std::process::exit(exit_code);
 }
 
-fn handle_build(build: Build) {
-
+fn handle_build(build: Build) -> i32 {
+    return 0;
 }
 
-fn handle_run() {
+fn handle_run() -> i32 {
     println!("Running oxos");
     Command::new("cargo")
     .stdout(Stdio::inherit())
@@ -54,10 +62,11 @@ fn handle_run() {
     .expect("Failed to start")
     .wait()
     .expect("Failed to run OxidizedOS");
-
+    
+    return 0;
 }
 
-fn handle_runner() {
+fn handle_runner(runner: Runner) -> i32 {
     println!("Generating iso...");
     // Make isodir
     Command::new("mkdir")
@@ -70,8 +79,10 @@ fn handle_runner() {
     .expect("Failed to crate isodir");
 
     // Copy binary
+    let input = runner.path;
+    let output = "isodir/boot/oxos.bin";
     Command::new("cp")
-    .args(&["target/x86_64-oxos/debug/oxos", "isodir/boot/oxos.bin"])
+    .args(&[&input, output])
     .stdout(Stdio::inherit())
     .stderr(Stdio::inherit())
     .spawn()
@@ -99,8 +110,8 @@ fn handle_runner() {
     .wait()
     .expect("Failed to generate oxos iso");
 
-    Command::new("qemu-system-x86_64")
-    .args(&["-smp", "4", "-cdrom", "oxos.iso", "-nographic", "--monitor", "none"])
+    let status = Command::new("qemu-system-x86_64")
+    .args(&["-smp", "4", "-cdrom", "oxos.iso", "-nographic", "--monitor", "none", "-device", "isa-debug-exit,iobase=0xf4,iosize=0x04"])
     .stdout(Stdio::inherit())
     .stdin(Stdio::inherit())
     .stderr(Stdio::inherit())
@@ -108,6 +119,47 @@ fn handle_runner() {
     .expect("Failed to start")
     .wait()
     .expect("Failed to run qemu");
+
+    let code: i64 = match status.code() {
+        Some(code) => code as i64,
+        None => EXIT_QEMU_SIGNAL
+    };
+
+    match code {
+        code if code == EXIT_QEMU_SUCCESS => 0,
+        _ => 1
+    }
     
-    
+}
+
+pub fn handle_clean() -> i32 {
+
+    Command::new("cargo")
+    .args(&["clean"])
+    .stdout(Stdio::inherit())
+    .stderr(Stdio::inherit())
+    .spawn()
+    .expect("Failed to start")
+    .wait()
+    .expect("Failed to run cargo clean");
+
+    Command::new("rm")
+    .args(&["-rf", "isodir"])
+    .stdout(Stdio::inherit())
+    .stderr(Stdio::inherit())
+    .spawn()
+    .expect("Failed to start")
+    .wait()
+    .expect("Failed to remove isodir");
+
+    Command::new("rm")
+    .args(&["-f", "oxos.iso"])
+    .stdout(Stdio::inherit())
+    .stderr(Stdio::inherit())
+    .spawn()
+    .expect("Failed to start")
+    .wait()
+    .expect("Failed to remove isodir");
+
+    return 0;
 }
